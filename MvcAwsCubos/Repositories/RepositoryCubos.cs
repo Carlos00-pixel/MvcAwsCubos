cde115af701c347bdd3be5c78b422269f9ae1158
Carlos00-pixel <58859997+Carlos00-pixel@users.noreply.github.com>
2023-05-30 13:11:29 +0200

    Add project files.

---
+++ MvcAwsCubos/Repositories/RepositoryCubos.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcAwsCubos.Data;
+using MvcAwsCubos.Models;
+
+namespace MvcAwsCubos.Repositories
+{
+    public class RepositoryCubos
+    {
+        private CubosContext context;
+
+        public RepositoryCubos(CubosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Cubo>> GetCubosAsync()
+        {
+            return await this.context.Cubos.ToListAsync();
+        }
+
+        public async Task<Cubo> FindCuboAsync(int id)
+        {
+            return await
+                this.context.Cubos
+                .FirstOrDefaultAsync(x => x.IdCubo == id);
+        }
+
+        private int GetMaxIdCubo()
+        {
+            return this.context.Cubos.Max(z => z.IdCubo) + 1;
+        }
+
+        public async Task CreateCuboAsync
+            (string nombre, string marca, string imagen, int precio)
+        {
+            Cubo cubo = new Cubo();
+            cubo.IdCubo = this.GetMaxIdCubo();
+            cubo.Nombre = nombre;
+            cubo.Marca = marca;
+            cubo.Imagen = imagen;
+            cubo.Precio = precio;
+            this.context.Cubos.Add(cubo);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCuboAsync
+            (int id, string nombre, string marca, string imagen, int precio)
+        {
+            Cubo cubo = await this.FindCuboAsync(id);
+            cubo.IdCubo = id;
+            cubo.Nombre = nombre;
+            cubo.Marca = marca;
+            cubo.Imagen = imagen;
+            cubo.Precio = precio;
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCuboAsync(int id)
+        {
+            Cubo cubo = await this.FindCuboAsync(id);
+            this.context.Cubos.Remove(cubo);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
